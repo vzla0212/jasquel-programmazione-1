fd790e63f168b48b0f8bd9b5f5e1aaed417c4a12
vzla0212 <elgordo18635985@gmail.com>
2012-02-09 15:57:53 -0530

    Update jj.hs

--- jj.hs
+++ jj.hs
@@ -44,7 +44,23 @@ foo a b c e = if elem (fst(head c)) a
                        then foo ((fst(head c)):a) b (tail c) ((head c):e)
                        else if ((fst(head c) > 100) && (elem (fst(head c)-99)  a))
                             then foo (delete ((fst(head c))-99) a) b (tail c) ((head c):e)
-                            else foo a b (tail c) ((head c):e)             
+                            else foo a b (tail c) ((head c):e
+
+
+{-
+Ante las modificaciones propuestas, en la cual se agregaba un constructor para manejar los becuadros, preferimos usar la
+solución que nosotros planteamos, la cual es explicada en el siguiente parrafo:
+
+Los becuadros van a ser representados por el entero midi más un entero razonable (100) que no entre en conflicto con el 
+conjunto de notas planteada en el enunciado que va desde 60 a 77; al ejecutar la operación de adición producirá enteros
+en el rango de 160 a 177 y este rango es disjunto al rango de notas, lo cual no ocasionara ningún tipo de mezcla entre
+ellas.
+
+Para lograr una mayor comprensión de lo anterior planteado, se exhiben los sguientes puntos:
+
+0) al restar 100 al entero midi que representa al becuadro se obtiene la nota sin ningún tipo de alteración.
+1) al restar 99 al entero midi que representa al becuadro se obtiene la nota, esta vez con la alteración de sostenido.
+-}
 
 tiempo :: String -> Tiempo
 tiempo a = (digitToInt (head a ) , 4)
